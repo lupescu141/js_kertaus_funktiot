4140405f736a5e677728f88f23fadb74be7ca036
Daniel Lupescu <d.lupescu@hotmail.fi>
2024-08-25 23:56:59 +0300

    final push

--- tehtava_6/main.js
+++ tehtava_6/main.js
@@ -1,1 +1,24 @@
+const movielist = [];
+const movie = {
+  tittle: "",
+  rating: "",
+};
+
 const movieSum = prompt("how many movies would you like to rate?");
+
+for (i = 1; i <= movieSum; i++) {
+  
+  movie.tittle = prompt("enter " + i + " movie tittle:");
+  movie.rating = prompt("enter " + i + " movie rating:");
+  movielist.push(JSON.parse(JSON.stringify(movie)));
+}
+
+movielist.sort((a, b) => b.rating - a.rating);
+console.log(movielist)
+
+let textnode = document.createTextNode(JSON.stringify(movielist));
+document.body.appendChild(textnode);
+const br = document.createElement("BR");
+document.body.appendChild(br);
+textnode = document.createTextNode("Highest rate movie: " + JSON.stringify(movielist[0]));
+document.body.appendChild(textnode);
